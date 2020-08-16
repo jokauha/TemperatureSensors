8523a3ac71cd81cc237c3e3af0a10f9c7f1f1e33
Joakim Kauhanen <joakim.kauhanen@gmail.com>
2020-08-16 23:06:09 +0200

    Tietokantamalli

--- backend/index.js
+++ backend/index.js
@@ -25,9 +25,44 @@ client.on('message', function (topic, message) {
                 .replace("olohuoneoutside", "ulko")
                 .replace("keittiojaakaappi", "jaakaappi")
 
-    temps.push({name: tempname, temperature: JSON.parse(message.toString()).temperature, date: new Date()})
+    // temps.push({name: tempname, temperature: JSON.parse(message.toString()).temperature, date: new Date()})
+    TemperatureModel.create({name: tempname, temperature: JSON.parse(message.toString()).temperature, date: new Date()})
+      //.then(item=>(console.log("success " + item)))
   }
 
   console.log(temps)
   // client.end()
 })
+
+const { Sequelize, DataTypes, Model } = require('sequelize');
+const sequelize = new Sequelize({
+  dialect: 'sqlite',
+  storage: './temperatures.sqlite'
+});
+
+class TemperatureModel extends Model {}
+
+TemperatureModel.init({
+  // Model attributes are defined here
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  temperature: {
+    type: DataTypes.DECIMAL,
+    allowNull: false
+  },
+  date: {
+    type: DataTypes.DATE,
+    allowNull: false
+  }
+}, {
+  // Other model options go here
+  sequelize, // We need to pass the connection instance
+  modelName: 'TemperatureModel', // We need to choose the model name
+  tableName: 'Temperatures',
+  timestamps: false
+});
+
+// the defined model is the class itself
+// console.log(TemperatureModel === sequelize.models.TemperatureModel); // true
